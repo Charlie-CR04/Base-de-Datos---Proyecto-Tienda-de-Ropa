015e91878587c20ee7bebec6beb335739b7d8643
Osvaldo Becerril Salgado <166682160+elbotasoswaldino@users.noreply.github.com>
2024-11-15 00:36:32 -0600

    Update Datos Proveedor.sql
    10 Datos agregados

--- Datos Proveedor.sql
+++ Datos Proveedor.sql
@@ -37,3 +37,44 @@ VALUES (9, 'Grupo de Manufactura Exclusiva',5534567892,
 INSERT INTO PROVEEDOR
 VALUES (10, 'Premium Manufacturas S.A.',5591023456,
 'info@premiummanuf.com.mx', 'Av. Revolución #1001, CDMX');
+
+INSERT INTO PROVEEDOR 
+VALUES (11, 'Textiles del Sur', 5587456123, 
+'ventas@textilessur.com.mx', 'Calle Libertad #43, Oaxaca');
+
+INSERT INTO PROVEEDOR 
+VALUES (12, 'Materiales de Alta Calidad', 5549832745, 
+'contacto@materialesac.com.mx', 'Av. Hidalgo #34, Morelia');
+
+INSERT INTO PROVEEDOR 
+VALUES (13, 'Suministros Técnicos S.A.', 5578912345, 
+'info@suministros-tecnicos.com.mx', 'Blvd. Manuel Ávila Camacho #124, CDMX');
+
+INSERT INTO PROVEEDOR 
+VALUES (14, 'Industrias del Norte', 5592347812, 
+'ventas@indelnorte.com.mx', 'Av. Patria #432, Tijuana');
+
+INSERT INTO PROVEEDOR 
+VALUES (15, 'Textiles y Confecciones Jalisco', 5526743981, 
+'contacto@textilesjal.com.mx', 'Calle Vallarta #98, Guadalajara');
+
+INSERT INTO PROVEEDOR 
+VALUES (16, 'Proveedora de Insumos Puebla', 5587123490, 
+'info@proveedorapuebla.com.mx', 'Calle Reforma #290, Puebla');
+
+INSERT INTO PROVEEDOR 
+VALUES (17, 'Distribuidora Maya', 5567123845, 
+'ventas@dismaya.com.mx', 'Av. Paseo Montejo #345, Mérida');
+
+INSERT INTO PROVEEDOR 
+VALUES (18, 'Insumos y Textiles Veracruz', 5534982734, 
+'contacto@insumosver.com.mx', 'Calle Independencia #12, Veracruz');
+
+INSERT INTO PROVEEDOR 
+VALUES (19, 'Corporación de Insumos Industriales', 5578123945, 
+'info@corpinsumosind.com.mx', 'Av. Cuauhtémoc #56, CDMX');
+
+INSERT INTO PROVEEDOR 
+VALUES (20, 'Manufacturas y Confecciones del Centro', 5541237890, 
+'contacto@mancentro.com.mx', 'Calle 5 de Mayo #321, San Luis Potosí');
+
